8e781778257ed52004cc4c3f71f233246607f3f5
Dima Neto <kuznetsovdv2013@gmail.com>
2025-05-31 19:16:59 +0300

    изменения структуры проекта, сделана логика перемещения и атаки персонажей, сделаны тесты

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -2,12 +2,14 @@ import themes from "./themes.js";
 import cursors from "./cursors.js";
 import { generateTeam } from "./generators.js";
 import { characterInfo } from "./utils.js";
+import { canMoveTo } from "./utils/canMoveTo.js";
+import { canAttack } from "./utils/canAttack.js";
 import PositionedCharacter from "./PositionedCharacter.js";
 import GameState from "./GameState.js";
 import Bowman from "./characters/bowman.js";
 import Swordsman from "./characters/swordsman.js";
 import Magician from "./characters/magician.js";
-import Daemon from "./characters/daemon.js";щ
+import Daemon from "./characters/daemon.js";
 import Undead from "./characters/undead.js";
 import Vampire from "./characters/vampire.js";
 import GamePlay from "./GamePlay.js";
@@ -18,7 +20,9 @@ export default class GameController {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
     this.allPositioned = [];
-    this.gameState = new GameState()
+    this.gameState = new GameState();
+    this.playerTypes = [Swordsman, Bowman, Magician];
+    this.enemyTypes = [Daemon, Undead, Vampire];
     this.onCellEnter = this.onCellEnter.bind(this);
     this.onCellLeave = this.onCellLeave.bind(this);
     this.onCellClick = this.onCellClick.bind(this);
@@ -57,34 +61,103 @@ export default class GameController {
     }
 
     const positionChar = this.allPositioned.find(pc => pc.position === index);
+
     if (!positionChar || !['bowman', 'swordsman', 'magician']
       .includes(positionChar.character.type)) {
         GamePlay.showError('Выберите своего персонажа');
         return;
       }
+
     if(this.selectedCellIndex !== undefined) {
-      this.gamePlay.deselectCell(this.selectedCellIndex)
+      this.gamePlay.deselectCell(this.selectedCellIndex);
     }
     
     this.selectedCellIndex = index;
     this.gamePlay.selectCell(index, 'yellow');
-  }
 
-  onCellEnter(index) {
-    const positionChar = this.allPositioned.find(pc => pc.position === index);
-    if (positionChar) {
-      const { level, attack, defence, health } = positionChar.character;
-      const tooltipText = characterInfo`${level} ${attack} ${defence} ${health}`;
-      this.gamePlay.showCellTooltip(tooltipText, index);
+    if(this.isInvalidAction(this.selectedCellIndex, index)) {
+      alert('Недопустимое действие, выберете другую клетку');
+      return;
     }
   }
 
-  onCellLeave(index) {
+
+  onCellEnter(index) {
+    const positionChar = this.allPositioned.find(pc => pc.position === index);
+
+//если персонаж не выбран
+    if(!this.selectedCellIndex) {
+      if (positionChar && ['bowman', 'swordsman', 'magician']
+        .includes(positionChar.character.type)) {
+        const { level, attack, defence, health } = positionChar.character;
+        const tooltipText = characterInfo`${level} ${attack} ${defence} ${health}`;
+        this.gamePlay.showCellTooltip(tooltipText, index);
+        this.gamePlay.setCursor(cursors.pointer);
+      } else {
+        this.gamePlay.hideCellTooltip(index);
+        this.gamePlay.setCursor(cursors.auto);
+      }
+    return;
+    }
+// если персонаж выбран, его подсветка не меняется
+    if(index === this.selectedCellIndex) {
+      if(positionChar) {
+        const { level, attack, defence, health } = positionChar.character;
+        const tooltipText = characterInfo`${level} ${attack} ${defence} ${health}`;
+        this.gamePlay.showCellTooltip(tooltipText, index);
+      }
+      this.gamePlay.setCursor(cursors.auto);
+      return;
+    }
+
+// если персонаж, выбран, но наводим курсор на другого персонажа
+    if (positionChar && ['bowman', 'swordsman', 'magician']
+      .includes(positionChar.character.type) 
+      && positionChar.position !== this.selectedCellIndex) {
+      const { level, attack, defence, health } = positionChar.character;
+      const tooltipText = characterInfo`${level} ${attack} ${defence} ${health}`;
+      this.gamePlay.showCellTooltip(tooltipText, index);
+      this.gamePlay.setCursor(cursors.pointer);
+      return;
+    }
+
+//условие перемещания
+    if(canMoveTo(this.allPositioned, this.selectedCellIndex, index)) {
+      this.gamePlay.setCursor(cursors.pointer);
+      this.gamePlay.selectCell(index, 'green');
+      this.gamePlay.hideCellTooltip(index);
+      return;
+    }
+
+//условие атаки
+    if(positionChar && this.enemyTypes.includes(positionChar.character.type)
+       && canAttack(this.allPositioned, this.selectedCellIndex, index)) {
+      this.gamePlay.setCursor(cursors.crosshair);
+      this.gamePlay.selectCell(index, 'red');
+      this.gamePlay.hideCellTooltip(index);
+      return;
+    }
+
+    this.gamePlay.setCursor(cursors.notallowed);
     this.gamePlay.hideCellTooltip(index);
   }
 
-}
+  onCellLeave(index) {
+    if (this.selectedCellIndex !== index) {
+    this.gamePlay.deselectCell(index);
+      }
+    this.gamePlay.setCursor(cursors.auto);
+    this.gamePlay.hideCellTooltip(index);
+  }
 
+  isInvalidAction(from, to) {
+    const attackPossible = canAttack(this.allPositioned, from, to);
+    const movePossible = canMoveTo(this.allPositioned, from, to);
+    return !(movePossible || attackPossible);
+  }
+
+}
+//получение позиции игрока
 function getPlayerPosition(boardSize, teamSize) {
   const positions = [];
     for (let i = 0; i < boardSize; i++) {
@@ -93,7 +166,7 @@ function getPlayerPosition(boardSize, teamSize) {
     }
     return positions.sort(() => Math.random() - 0.5).slice(0, teamSize);
 }
-
+//получение позиции врага
 function getEnemyPosition(boardSize, teamSize) {
   const positions = [];
     for (let i = 0; i < boardSize; i++) {
---
+++ src/js/utils/canMoveTo.js
@@ -0,0 +1,33 @@
+export function canMoveTo(allPositioned, from, to) {
+  const fromX = from % 8;
+  const fromY = Math.floor(from / 8);
+  const toX = to % 8;
+  const toY = Math.floor(to / 8);
+
+  const deltaX = toX - fromX;
+  const deltaY = toY - fromY;
+
+  const isStraight = (deltaX === 0 || deltaY === 0);
+  const isDiagonal = Math.abs(deltaX) === Math.abs(deltaY);
+
+  if (!isStraight && !isDiagonal) {
+    return false;
+  }
+
+  const movingCharObj = allPositioned.find(pc => pc.position === from);
+  if (!movingCharObj) return false;
+
+  const type = movingCharObj.character.type;
+
+  let maxDistance = 0;
+
+  if (['swordsman', 'undead'].includes(type)) maxDistance = 4;
+  else if (['bowman', 'vampire'].includes(type)) maxDistance = 2;
+  else if (['magician', 'daemon'].includes(type)) maxDistance = 1;
+  else return false;
+
+  const distance = Math.max(Math.abs(deltaX), Math.abs(deltaY));
+  
+  return distance <= maxDistance;
+ 
+}
